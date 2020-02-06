40a8ab42fabdcfa5a76a9ab0f00fe3714e813c31
mhorava <mhorava@users.noreply.github.com>
2020-02-06 21:54:27 +0000

    Update test keys

--- test/end-to-end-testapp/settings.js
+++ test/end-to-end-testapp/settings.js
@@ -1,6 +1,6 @@
 var SDKsettings = {
-    apiKey: 'key_live_phqFN7svok1v1Eo0X12gCcpbyBldcahC',
-    liveKey: 'key_live_phqFN7svok1v1Eo0X12gCcpbyBldcahC'
+    liveKey: 'key_live_phqFN7svok1v1Eo0X12gCcpbyBldcahC',
+    testKey: 'key_test_koxAQ1CsknYC7vb6819wfphcCrpkiplL'
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
     into the src/initialization.js file as the
