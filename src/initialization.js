93b5b65e0305d034f2f9973cfd4619be04160daf
mhorava <mhorava@users.noreply.github.com>
2020-02-14 14:14:02 +0000

    Fix async race condition-y thing

--- src/initialization.js
+++ src/initialization.js
@@ -4,19 +4,20 @@ var initialization = {
         if (!!window.branch) {
             return;
         }
+
         var branchScript = document.createElement('script');
         branchScript.type = 'text/javascript';
         branchScript.async = true;
         branchScript.src = 'https://cdn.branch.io/branch-latest.min.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(branchScript);
-        (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
         branchScript.onload = function(){
+            (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
             // use the test key if testMode
             var key = testMode ? settings.testKey : settings.liveKey;
             branch.init(key, function(err, data) {
             // callback to handle err or data
                 isInitialized = true;
-                console.log("isInitialized: " + isInitialized.toString());
+                branch.logEvent("Test");
             });
         }
     }
--- test/end-to-end-testapp/build/compilation.js
+++ test/end-to-end-testapp/build/compilation.js
@@ -444,15 +444,12 @@ CommerceHandler.prototype.logCommerceEvent = function(event) {
         ...event.UserAttributes
     };
 
-
-    "USD" ? (event_data_and_custom_data["currency"] = "USD") : console.log("");
-    // event.CurrencyCode ? (event_data_and_custom_data["currency"] = event.CurrencyCode) : console.log("");
+    !!event.CurrencyCode ? (event_data_and_custom_data["currency"] = event.CurrencyCode) : console.log("");
 
     // Turn ProductList into Branch content_items
     var content_items = event.ProductAction.ProductList.map(value => {
         return {
             $product_brand: value.Brand,
-            // $product_category: value.Category,
             $coupon_code: value.CouponCode,
             $product_name: value.Name,
             $price: value.Price,
@@ -560,19 +557,20 @@ var initialization = {
         if (!!window.branch) {
             return;
         }
+
         var branchScript = document.createElement('script');
         branchScript.type = 'text/javascript';
         branchScript.async = true;
         branchScript.src = 'https://cdn.branch.io/branch-latest.min.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(branchScript);
-        (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
         branchScript.onload = function(){
+            (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
             // use the test key if testMode
             var key = testMode ? settings.testKey : settings.liveKey;
             branch.init(key, function(err, data) {
             // callback to handle err or data
                 isInitialized = true;
-                console.log("isInitialized: " + isInitialized.toString());
+                branch.logEvent("Test");
             });
         }
     }
@@ -624,8 +622,8 @@ module.exports = UserAttributeHandler;
 
 },{}],12:[function(require,module,exports){
 var SDKsettings = {
-    liveKey: 'key_live_phqFN7svok1v1Eo0X12gCcpbyBldcahC',
-    testKey: 'key_test_koxAQ1CsknYC7vb6819wfphcCrpkiplL'
+    liveKey: 'key_live_phqFN7svok1v1Eo0X12gCcpbyBldcahC'
+    // testKey: 'key_test_koxAQ1CsknYC7vb6819wfphcCrpkiplL'
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
     into the src/initialization.js file as the
