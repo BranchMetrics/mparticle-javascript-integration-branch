bd3a58a89aa9eb78633924584da91bcf814e73e3
mhorava <mhorava@users.noreply.github.com>
2020-03-06 16:21:42 +0000

    Fix identity handling bug and add support for more identity types

--- src/identity-handler.js
+++ src/identity-handler.js
@@ -2,9 +2,37 @@ function IdentityHandler(common) {
     this.common = common || {};
 }
 
+
+"", "MPID", "Email", "Other", "Other2", "Other3", "Other4"
 function identified(mParticleUser, identityApiRequest) {
-    var mPUser = mParticleUser.getUserIdentities();
-    var userId = mPUser.customerid || mPUser.other || mPUser.other2 || mPUser.other3 || mPUser.other4;
+    var mPUser = mParticleUser.getUserIdentities().userIdentities;
+    switch (this.common.settings.userIdentificationType) {
+      case ('CustomerId'):
+        var userId = mPUser.customerid;
+        break;
+      case ('MPID'):
+        var userId = mParticleUser.getMPID();
+        break;
+      case ('Email'):
+        var userId = mPUser.email;
+        break;
+      case ('Other'):
+        var userId = mPUser.other;
+        break;
+      case ('Other2'):
+        var userId = mPUser.other2;
+        break;
+      case ('Other3'):
+        var userId = mPUser.other3;
+        break;
+      case ('Other4'):
+        var userId = mPUser.other4;
+        break;
+      default:
+        var userId = mPUser.customerid;
+        break;
+    }
+
     if (typeof userId !== 'undefined') {
         branch.setIdentity(userId);
     }
